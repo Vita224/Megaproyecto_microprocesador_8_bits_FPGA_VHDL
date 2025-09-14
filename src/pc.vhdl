9bce0395884d7cbb333933cee845121937e37b8b
Vita224 <53021236+Vita224@users.noreply.github.com>
2025-09-14 17:24:21 -0600

    Update pc.vhdl

--- src/pc.vhdl
+++ src/pc.vhdl
@@ -1,37 +1,34 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity pc is 
     port(
-        clock: in STD_LOGIC; -- Señal de reloj
-        reset: in STD_LOGIC; -- Señal de reinicio
-        en: in STD_LOGIC; -- Señal de habilitación del contador
-        oe: in STD_LOGIC; -- Señal de habilitación de la salida
-        ld: in STD_LOGIC; -- Señal de carga
-        input: in STD_LOGIC_VECTOR(3 downto 0); -- Entrada de datos para carga
-        output: out STD_LOGIC_VECTOR(3 downto 0) -- Salida del contador
+        clock: in STD_LOGIC;
+        reset: in STD_LOGIC;
+        en: in STD_LOGIC;
+        oe: in STD_LOGIC;
+        ld: in STD_LOGIC;
+        input: in STD_LOGIC_VECTOR(3 downto 0);
+        output: out STD_LOGIC_VECTOR(3 downto 0)
     );
 end entity;
 
--- Arquitectura -- 
 architecture behave of pc is
-    signal count: STD_LOGIC_VECTOR(3 downto 0) := "0000"; -- Declara una senal count de 4 bits que actua como el contador principal.
-	
+    signal count: unsigned(3 downto 0) := "0000";
 begin
     process(clock, reset)
     begin
         if reset = '1' then
-            count <= "0000";
+            count <= (others => '0');
         elsif rising_edge(clock) then
             if ld = '1' then
-                count <= input;
+                count <= unsigned(input);
             elsif en = '1' then
                 count <= count + 1;
             end if;
         end if;
     end process;
 
-    output <= count when oe = '1' else "ZZZZ";
-
+    output <= std_logic_vector(count) when oe = '1' else "ZZZZ";
 end behave;
