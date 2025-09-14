259da7c384c95f71a46b3bdf039a248b73bf2c30
Vita224 <53021236+Vita224@users.noreply.github.com>
2025-09-14 16:44:19 -0600

    Update debounce_v1.vhd

--- src/debounce_v1.vhd
+++ src/debounce_v1.vhd
@@ -8,23 +8,23 @@ entity debounce is
     );
     port(
         clock: in STD_LOGIC;  -- Reloj de entrada
-        button: in STD_LOGIC;  -- Entrada de la señal que necesita debounce
-        result: out STD_LOGIC -- Salida de la señal limpia (sin rebotes)
+        button: in STD_LOGIC;  -- Entrada de la senal que necesita debounce
+        result: out STD_LOGIC -- Salida de la senal limpia (sin rebotes)
     );
 end entity;
 
 architecture logic of debounce is
     signal flipflops: STD_LOGIC_VECTOR(1 downto 0); -- Vector de dos flip-flops 
-    signal counter_set: STD_LOGIC; -- Señal que indica cuándo reiniciar el contador
-    signal counter_out: STD_LOGIC_VECTOR(counter_size downto 0) := (others => '0'); -- Incrementa con cada pulso de reloj hasta que la señal de entrada esté estable
+    signal counter_set: STD_LOGIC; -- Senal que indica cuando reiniciar el contador
+    signal counter_out: STD_LOGIC_VECTOR(counter_size downto 0) := (others => '0'); -- Incrementa con cada pulso de reloj hasta que la senal de entrada este estable
 
 begin
-    counter_set <= flipflops(0) xor flipflops(1); -- Se activa cuando hay un cambio en la señal del botón
+    counter_set <= flipflops(0) xor flipflops(1); -- Se activa cuando hay un cambio en la senal del boton
   
     process(clock)
     begin
         if (clock'EVENT and clock = '1') then
-            flipflops(0) <= button; -- Se actualiza con la señal button
+            flipflops(0) <= button; -- Se actualiza con la senal button
             flipflops(1) <= flipflops(0); -- Esto crea un retardo en el que se detectan cambios en button
 
             if (counter_set = '1') then 
