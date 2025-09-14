31e81dbf83b3a0d0c63e6f7fb3b6308f437de29c
Vita224 <53021236+Vita224@users.noreply.github.com>
2025-09-14 17:18:08 -0600

    Update alu.vhdl

--- src/alu.vhdl
+++ src/alu.vhdl
@@ -1,38 +1,32 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL; -- Manejo de senales logicas
-use IEEE.NUMERIC_STD.ALL; -- Funciones para operaciones aritmeticas en VHDL
-use IEEE.STD_LOGIC_UNSIGNED.ALL; -- Trabajar operaciones aritmeticas y vectores sin signo
-use IEEE.STD_LOGIC_ARITH.ALL; -- Trabajar operaciones aritmeticas y vectores sin signo
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity alu is
     port(
         en: in STD_LOGIC; -- Habilita la salida de la ALU
-        op: in STD_LOGIC; -- Operacion a realizar (0 para suma, 1 para resta)
-        reg_a_in: in STD_LOGIC_VECTOR(7 downto 0); -- Operando A (8 bits)
-        reg_b_in: in STD_LOGIC_VECTOR(7 downto 0); -- Operando B (8 bits)
-        carry_out: out STD_LOGIC; -- Indicador de acarreo en la operacion
-        zero_flag: out STD_LOGIC; -- Indica si el resultado es 0
-        result_out: out STD_LOGIC_VECTOR(7 downto 0) -- El resultado de la operacion (8 bits)
+        op: in STD_LOGIC; -- Operación a realizar (0 para suma, 1 para resta)
+        reg_a_in: in STD_LOGIC_VECTOR(7 downto 0); -- Operando A
+        reg_b_in: in STD_LOGIC_VECTOR(7 downto 0); -- Operando B
+        carry_out: out STD_LOGIC;
+        zero_flag: out STD_LOGIC;
+        result_out: out STD_LOGIC_VECTOR(7 downto 0)
     );
 end entity;
 
--- Aqui se define el tipo de arquitectura "behave" que describe el comportamiento de la ALU
 architecture behave of alu is 
-    signal result: STD_LOGIC_VECTOR(8 downto 0); -- Señal interna de 9 bits para incluir carry
-	
+    signal result: unsigned(8 downto 0);
 begin    
-    -- Proceso de operacion: Se activa cuando reg_a_in, reg_b_in u op cambian
     process(reg_a_in, reg_b_in, op)
     begin
         if op = '0' then
-            result <= ext(reg_a_in, 9) + ext(reg_b_in, 9);
-        elsif op = '1' then
-            result <= ext(reg_a_in, 9) - ext(reg_b_in, 9);
+            result <= resize(unsigned(reg_a_in), 9) + resize(unsigned(reg_b_in), 9);
+        else
+            result <= resize(unsigned(reg_a_in), 9) - resize(unsigned(reg_b_in), 9);
         end if;
     end process;
 
-    -- Salidas
-    carry_out <= result(8); -- Muestra el bit mas significativo de result (posición 8) lo que indica si hubo un acarreo en la operación
-    zero_flag <= '1' when result(7 downto 0) = "00000000" else '0'; -- Activa la señal a 1 si el resultado (sin considerar el bit de acarreo) es 00000000
-    result_out <= result(7 downto 0) when en = '1' else (others => 'Z'); -- Muestra el resultado (los 8 bits menos significativos de result) si "en" es "1"; de lo contrario la salida se pone en alta impedancia.  
+    carry_out <= result(8);
+    zero_flag <= '1' when result(7 downto 0) = "00000000" else '0';
+    result_out <= std_logic_vector(result(7 downto 0)) when en = '1' else (others => 'Z');  
 end behave;
