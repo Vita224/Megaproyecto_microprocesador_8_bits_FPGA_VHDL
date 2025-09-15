f13f8cc44e207c04ed6aee760e872a3650ba80f5
Vita224 <53021236+Vita224@users.noreply.github.com>
2025-09-15 12:33:42 -0600

    Update reg.vhdl

--- src/reg.vhdl
+++ src/reg.vhdl
@@ -14,12 +14,12 @@ entity reg is
 end entity;
 
 architecture behave of reg is
-    signal stored_value: STD_LOGIC_VECTOR(7 downto 0) := (others => 'Z');
+    signal stored_value: STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
 begin
     process(clock, reset)
     begin
         if reset = '1' then
-            stored_value <= (others => 'Z');
+            stored_value <= (others => '0');
         elsif rising_edge(clock) then
             if load = '1' then
                 stored_value <= input;
@@ -27,6 +27,7 @@ begin
         end if;
     end process;
 
-    output <= stored_value when out_en = '1' else (others => 'Z');
+    -- Salidas siempre conducidas. El CPU decide vía señales de control cuál usar.
+    output <= stored_value;
     output_alu <= stored_value;
 end behave;
