d77b518899d8c1da443e95ea55f3c5f6d317d9a7
Vita224 <53021236+Vita224@users.noreply.github.com>
2025-09-15 12:35:01 -0600

    Update mar.vhdl

--- src/mar.vhdl
+++ src/mar.vhdl
@@ -12,12 +12,12 @@ entity mar is
 end entity;
 
 architecture behave of mar is
-    signal stored_value: STD_LOGIC_VECTOR(3 downto 0) := (others => 'Z');
+    signal stored_value: STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
 begin
     process(clock, reset)
     begin
         if reset = '1' then
-            stored_value <= (others => 'Z');
+            stored_value <= (others => '0');
         elsif rising_edge(clock) then
             if load = '1' then
                 stored_value <= input;
