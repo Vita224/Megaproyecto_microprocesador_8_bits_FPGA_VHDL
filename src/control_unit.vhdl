56ae7e79810c7a1bad946d18ef3f0ac680726963
Vita224 <53021236+Vita224@users.noreply.github.com>
2025-09-14 22:24:53 -0600

    Update control_unit.vhdl

--- src/control_unit.vhdl
+++ src/control_unit.vhdl
@@ -4,17 +4,18 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity control_unit is
     Port ( 
-        clock: in std_logic;
-        reset: in std_logic;
-        instr: in std_logic_vector(3 downto 0);
-        do : out std_logic_vector(16 downto 0)
+        clock : in std_logic;
+        reset : in std_logic;
+        instr : in std_logic_vector(3 downto 0);
+        do    : out std_logic_vector(16 downto 0)
     );
 end entity;
 
 architecture behave of control_unit is
     signal counter: std_logic_vector(3 downto 0) := "0000";
-	
 begin
+
+    -- Contador de ciclos
     count_proc: process(clock, reset)
     begin
         if reset = '1' then
@@ -28,64 +29,89 @@ begin
         end if;
     end process;
 
--- Este componente se encarga de ejecutar los microcodigos, estos dependen de la instruccion almacenada en la RAM y del counter que se ejecuta en cada pulso de reloj.
--- Dando como salida una mascara que habilita o no la salida, entrada en los diferentes componentes de nuestro microcontrolador.
--- Cada microcodigo se utiliza por ejemplo para mover un valor de la RAM a uno de los registros, sumar y restar valores, realizar un bucle en las instrucciones y demas instrucciones que le programemos. 
--- 
+    -- Generador de microcodigos
+    microcode_proc: process(counter, instr)
+    begin
+        -- valor por defecto (evita latches)
+        do <= (others => '0');
 
-    --|HLT|OUT_IN|OUT_OUT|ALU_OP_EN|ALU_EN|PC_EN|PC_IN|PC_OUT|
-    --|16 |  15  |   14  |    13   |  12  |  11 | 10  |  9   |
-    --|MAR_IN|RAM_IN|RAM_OUT|A_IN|A_OUT|B_IN|B_OUT|instr_I|instr_OUT|
-    --|  8   |  7   |   6   |  5 |  4  |  3 |  2  |   1  |    0   |
+        case counter is
+            ------------------------------------------------------------------
+            when "0000" =>
+                do <= "00000001100000000"; -- PC_OUT - MAR_IN (9 - 8)
 
-    do <= "00000001100000000" when counter = "0000" else -- PC_OUT - MAR_IN (9 - 8)   300
-          "00000100001000010" when counter = "0001" else -- PC_EN - RAM_OUT - instr_I (11 - 6 - 1)   842
-          
-          -- LDA
-          "00000000000000001" when (counter = "0010" and instr = "0000") else -- instr_OUT (0)   1
-          "00000000100000001" when (counter = "0011" and instr = "0000") else -- MAR_IN - instr_OUT (8 - 0)    101
-          "00000000001100000" when (counter = "0100" and instr = "0000") else -- RAM_OUT - A_IN (6 - 5)     60
-          "00000000000000000" when (counter = "0101" and instr = "0000") else
-          "00000000000000000" when (counter = "0110" and instr = "0000") else
-          
-          -- STA 
-          "00000000000000001" when (counter = "0010" and instr = "0001") else   -- instr_OUT
-          "00000000100000001" when (counter = "0011" and instr = "0001") else   -- MAR_IN + instr_OUT
-          "00000000010010000" when (counter = "0100" and instr = "0001") else   -- A_OUT + RAM_IN
-          
-          -- ADD         
-          "00000000000000001" when (counter = "0010" and instr = "0010") else -- instr_OUT (0)
-          "00000000100000001" when (counter = "0011" and instr = "0010") else -- MAR_IN - instr_OUT (8 - 0)
-          "00000000001001000" when (counter = "0100" and instr = "0010") else -- RAM_OUT - B_IN (6 - 3)
-          "00001000000000000" when (counter = "0101" and instr = "0010") else -- ALU_EN (12)
-          "00001000000100000" when (counter = "0110" and instr = "0010") else -- ALU_EN - A_IN (12 - 5)
-          
-          -- SUB         
-          "00000000000000001" when (counter = "0010" and instr = "0011") else -- instr_OUT (0)
-          "00000000100000001" when (counter = "0011" and instr = "0011") else -- MAR_IN - instr_OUT (8 - 0)
-          "00000000001001000" when (counter = "0100" and instr = "0011") else -- RAM_OUT - B_IN (6 - 3)
-          "00010000000000000" when (counter = "0101" and instr = "0011") else -- ALU_OP_EN (13)
-          "00011000000100000" when (counter = "0110" and instr = "0011") else -- ALU_OP_EN - ALU_EN - A_IN (13 - 12 - 5)
-          
-          -- JMP         
-          "00000000000000001" when (counter = "0010" and instr = "0100") else -- instr_OUT (0)
-          "00000010000000001" when (counter = "0011" and instr = "0100") else -- MAR_IN - instr_OUT (10 - 0)
-          "00000000000000000" when (counter = "0100" and instr = "0100") else    
-          "00000000000000000" when (counter = "0101" and instr = "0100") else    
-          "00000000000000000" when (counter = "0110" and instr = "0100") else
-          
-          -- OUT         
-          "00000000000000001" when (counter = "0010" and instr = "0101") else -- instr_OUT (0)
-          "00000000000010000" when (counter = "0011" and instr = "0101") else -- A_OUT (4)
-          "01000000000010000" when (counter = "0100" and instr = "0101") else -- OUT_IN - A_OUT (15 - 4)
-          "00100000000000000" when (counter = "0110" and instr = "0101") else -- OUT_OUT (14)
-          "00100000000000000" when (counter = "0110" and instr = "0101") else -- OUT_OUT (14)
-          
-          -- HLT         
-          "10000000000000000" when (counter = "0010" and instr = "0110") else    
-          "10000010000000000" when (counter = "0011" and instr = "0110") else
-          "00000000000000000" when (counter = "0100" and instr = "0110") else    
-          "00000000000000000" when (counter = "0101" and instr = "0110") else    
-          "00000000000000000" when (counter = "0110" and instr = "0110");
+            when "0001" =>
+                do <= "00000100001000010"; -- PC_EN - RAM_OUT - instr_I
+
+            ------------------------------------------------------------------
+            -- LDA
+            when "0010" =>
+                if instr = "0000" then
+                    do <= "00000000000000001"; -- instr_OUT
+                elsif instr = "0001" then
+                    do <= "00000000000000001"; -- STA: instr_OUT
+                elsif instr = "0010" then
+                    do <= "00000000000000001"; -- ADD: instr_OUT
+                elsif instr = "0011" then
+                    do <= "00000000000000001"; -- SUB: instr_OUT
+                elsif instr = "0100" then
+                    do <= "00000000000000001"; -- JMP: instr_OUT
+                elsif instr = "0101" then
+                    do <= "00000000000000001"; -- OUT: instr_OUT
+                elsif instr = "0110" then
+                    do <= "10000000000000000"; -- HLT
+                end if;
+
+            when "0011" =>
+                if instr = "0000" then
+                    do <= "00000000100000001"; -- LDA: MAR_IN - instr_OUT
+                elsif instr = "0001" then
+                    do <= "00000000100000001"; -- STA
+                elsif instr = "0010" then
+                    do <= "00000000100000001"; -- ADD
+                elsif instr = "0011" then
+                    do <= "00000000100000001"; -- SUB
+                elsif instr = "0100" then
+                    do <= "00000010000000001"; -- JMP
+                elsif instr = "0101" then
+                    do <= "00000000000010000"; -- OUT: A_OUT
+                elsif instr = "0110" then
+                    do <= "10000010000000000"; -- HLT
+                end if;
+
+            when "0100" =>
+                if instr = "0000" then
+                    do <= "00000000001100000"; -- LDA: RAM_OUT - A_IN
+                elsif instr = "0001" then
+                    do <= "00000000010010000"; -- STA: A_OUT + RAM_IN
+                elsif instr = "0010" then
+                    do <= "00000000001001000"; -- ADD: RAM_OUT - B_IN
+                elsif instr = "0011" then
+                    do <= "00000000001001000"; -- SUB: RAM_OUT - B_IN
+                elsif instr = "0101" then
+                    do <= "01000000000010000"; -- OUT_IN - A_OUT
+                end if;
+
+            when "0101" =>
+                if instr = "0010" then
+                    do <= "00001000000000000"; -- ADD: ALU_EN
+                elsif instr = "0011" then
+                    do <= "00010000000000000"; -- SUB: ALU_OP_EN
+                end if;
+
+            when "0110" =>
+                if instr = "0010" then
+                    do <= "00001000000100000"; -- ADD: ALU_EN - A_IN
+                elsif instr = "0011" then
+                    do <= "00011000000100000"; -- SUB: ALU_OP_EN - ALU_EN - A_IN
+                elsif instr = "0101" then
+                    do <= "00100000000000000"; -- OUT_OUT
+                end if;
+
+            ------------------------------------------------------------------
+            when others =>
+                do <= (others => '0');
+        end case;
+    end process;
 
 end behave;
